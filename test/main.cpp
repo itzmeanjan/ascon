0a2b8c0dfdb0402b988fd74c10b4273b164669cc
Anjan Roy <anjanroy@yandex.com>
2022-03-27 10:05:36 +0000

    minimally test Ascon hash, hashA implementation

--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,4 @@
+#include "test_hash.hpp"
 #include "test_permutation.hpp"
 #include <iostream>
 
@@ -6,6 +7,12 @@ main(int argc, char** argv)
 {
   ascon_test::p_a();
   std::cout << "[test] passed ascon permutation `p_a`" << std::endl;
+
+  ascon_test::hash();
+  std::cout << "[test] passed ascon hash" << std::endl;
+
+  ascon_test::hash_a();
+  std::cout << "[test] passed ascon hashA" << std::endl;
 
   return EXIT_SUCCESS;
 }
