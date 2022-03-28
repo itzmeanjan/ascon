eb2d13698b17291411a354835c3745152fdff234
Anjan Roy <anjanroy@yandex.com>
2022-03-28 14:43:50 +0000

    decrypt ascon cipher text

--- include/auth_enc.hpp
+++ include/auth_enc.hpp
@@ -385,7 +385,7 @@ template<const size_t b, const size_t r>
 static inline void
 process_plaintext(uint64_t* const __restrict state,
                   const uint8_t* const __restrict text,
-                  const size_t text_len,
+                  const size_t text_len, // in terms of bytes
                   uint8_t* const __restrict cipher) requires(check_b(b) &&
                                                              check_r(r))
 {
@@ -452,6 +452,114 @@ process_plaintext(uint64_t* const __restrict state,
           cipher_[i] = static_cast<uint8_t>((state[1] >> ((15ul - i) << 3)));
         }
       }
+    }
+  }
+}
+
+// Process cipher text in blocks ( same as rate bits wide ) and keep producing
+// plain text blocks is equal sized blocks; see section 2.4.3 of Ascon
+// specification
+// https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/ascon-spec-final.pdf
+template<const size_t b, const size_t r>
+static inline void
+process_ciphertext(uint64_t* const __restrict state,
+                   const uint8_t* const __restrict cipher,
+                   const size_t cipher_len, // in terms of bytes
+                   uint8_t* const __restrict text) requires(check_b(b) &&
+                                                            check_r(r))
+{
+  const size_t cipher_bit_len = cipher_len << 3;
+  const size_t cipher_blocks = cipher_bit_len / r;
+  const size_t remaining_bit_len = cipher_bit_len % r;
+
+  for (size_t i = 0; i < cipher_blocks; i++) {
+    if (r == 64) {
+      const size_t offset = i << 3;
+
+      const uint64_t cipher_blk = from_be_bytes(cipher + offset);
+      const uint64_t text_blk = cipher_blk ^ state[0];
+
+      to_be_bytes(text_blk, text + offset);
+
+      state[0] = cipher_blk;
+      p_b<b>(state);
+    } else if (r == 128) {
+      const size_t offset_0 = (i << 1) << 3;
+      const size_t offset_1 = ((i << 1) + 1) << 3;
+
+      const uint64_t cipher_blk_0 = from_be_bytes(cipher + offset_0);
+      const uint64_t cipher_blk_1 = from_be_bytes(cipher + offset_1);
+
+      const uint64_t text_blk_0 = cipher_blk_0 ^ state[0];
+      const uint64_t text_blk_1 = cipher_blk_1 ^ state[1];
+
+      to_be_bytes(text_blk_0, text + offset_0);
+      to_be_bytes(text_blk_1, text + offset_1);
+
+      state[0] = cipher_blk_0;
+      state[1] = cipher_blk_1;
+      p_b<b>(state);
+    }
+  }
+
+  if (remaining_bit_len > 0) {
+    const size_t rem_byte_len = remaining_bit_len >> 3;
+    const uint8_t* cipher_ = cipher + cipher_len - rem_byte_len;
+    uint8_t* text_ = text + cipher_len - rem_byte_len;
+
+    if (r == 64) {
+      uint64_t rem_cipher = 0ul;
+      for (size_t i = 0; i < rem_byte_len; i++) {
+        rem_cipher |= static_cast<uint64_t>(cipher_[i]) << ((7ul - i) << 3);
+      }
+
+      const uint64_t rem_text = state[0] ^ rem_cipher;
+
+      for (size_t i = 0; i < rem_byte_len; i++) {
+        text_[i] = static_cast<uint8_t>((rem_text >> ((7ul - i) << 3)));
+      }
+
+      state[0] ^= (rem_text | (0b1ul << (((8ul - rem_byte_len) << 3) - 1ul)));
+    } else if (r == 128) {
+      uint64_t rem_cipher_0 = 0ul;
+      uint64_t rem_cipher_1 = 0ul;
+      for (size_t i = 0; i < rem_byte_len; i++) {
+        if (i < 8) {
+          rem_cipher_0 |= static_cast<uint64_t>(cipher_[i]) << ((7 - i) << 3);
+        } else {
+          rem_cipher_1 |= static_cast<uint64_t>(cipher_[i]) << ((15 - i) << 3);
+        }
+      }
+
+      const uint64_t rem_text_0 = state[0] ^ rem_cipher_0;
+      const uint64_t rem_text_1 = state[1] ^ rem_cipher_1;
+
+      for (size_t i = 0; i < rem_byte_len; i++) {
+        if (i < 8) {
+          text_[i] = static_cast<uint8_t>((rem_text_0 >> ((7 - i) << 3)));
+        } else {
+          text_[i] = static_cast<uint8_t>((rem_text_1 >> ((15 - i) << 3)));
+        }
+      }
+
+      if (rem_byte_len < 8) {
+        state[0] ^= (rem_text_0 | (0b1ul << (((8 - rem_byte_len) << 3) - 1)));
+        state[1] ^= 0b0ul;
+      } else if (rem_byte_len == 8) {
+        state[0] ^= rem_text_0;
+        state[1] ^= (0b1ul << 63);
+      } else {
+        state[0] ^= rem_text_0;
+        state[1] ^= (rem_text_1 | (0b1ul << (((16 - rem_byte_len) << 3) - 1)));
+      }
+    }
+
+  } else {
+    if (r == 64) {
+      state[0] ^= 0b1ul << 63;
+    } else if (r == 128) {
+      state[0] ^= 0b1ul << 63;
+      state[1] ^= 0b0ul;
     }
   }
 }
--- include/utils.hpp
+++ include/utils.hpp
@@ -3,7 +3,7 @@
 
 // Given big-endian byte array of length 8, this function interprets it as
 // 64 -bit unsigned integer
-inline const uint64_t
+static inline const uint64_t
 from_be_bytes(const uint8_t* const i_bytes)
 {
   return (static_cast<uint64_t>(i_bytes[0]) << 56) |
@@ -18,11 +18,26 @@ from_be_bytes(const uint8_t* const i_bytes)
 
 // Given a 64 -bit unsigned integer, this function interprets it as a big-endian
 // byte array
-inline void
+static inline void
 to_be_bytes(const uint64_t num, uint8_t* const bytes)
 {
 #pragma unroll 8
   for (size_t i = 0; i < 8; i++) {
     bytes[i] = static_cast<uint8_t>(num >> ((8u - (i + 1u)) << 3u));
   }
+}
+
+// Binary logarithm of n, when n = 2 ^ i | i = {1, 2, 3, ...}; taken from
+// https://github.com/itzmeanjan/blake3-fpga/blob/a9aa7371ad492c5b01c4aa060982502acbbdef67/include/blake3.hpp#L33-L45
+static inline const size_t
+bin_log(size_t n)
+{
+  size_t cnt = 0ul;
+
+  while (n > 1ul) {
+    n >>= 1;
+    cnt++;
+  }
+
+  return cnt;
 }
