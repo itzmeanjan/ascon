a9cee96d6c5e13e5b86a80249ab422ab00576c98
Anjan Roy <anjanroy@yandex.com>
2022-03-29 10:11:18 +0000

    test `ascon-{128,128a}` implementations using Known Answer Tests

--- test.sh
+++ test.sh
@@ -6,6 +6,8 @@ make lib
 # bring in Ascon python implementation which can generate Known Answer Tests
 git clone https://github.com/meichlseder/pyascon.git
 
+# --- --- ---- --- --- #
+
 # generate KAT for Ascon Hash
 pushd pyascon
 
@@ -20,9 +22,11 @@ mv LWC_HASH_KAT_256.txt wrapper/python
 # test Ascon Hash implementation
 pushd wrapper/python
 
-pytest -k hash_kat --cache-clear -v
+pytest -k hash_kat --cache-clear -v -s
 
 popd
+
+# --- --- ---- --- --- #
 
 # generate KAT for Ascon HashA
 pushd pyascon
@@ -37,10 +41,52 @@ mv LWC_HASH_KAT_256.txt wrapper/python
 # test Ascon HashA implementation
 pushd wrapper/python
 
-pytest -k hashA_kat --cache-clear -v
+pytest -k hashA_kat --cache-clear -v -s
 rm LWC_HASH_KAT_256.txt
 
 popd
+
+# --- --- ---- --- --- #
+
+# generate KAT for Ascon-128
+pushd pyascon
+
+python3 genkat.py Ascon-128
+mv LWC_AEAD_KAT_128_128.txt ..
+
+popd
+
+mv LWC_AEAD_KAT_128_128.txt wrapper/python
+
+# test Ascon-128 implementation
+pushd wrapper/python
+
+pytest -k ascon_128_kat --cache-clear -v -s
+rm LWC_AEAD_KAT_128_128.txt
+
+popd
+
+# --- --- ---- --- --- #
+
+# generate KAT for Ascon-128a
+pushd pyascon
+
+python3 genkat.py Ascon-128a
+mv LWC_AEAD_KAT_128_128.txt ..
+
+popd
+
+mv LWC_AEAD_KAT_128_128.txt wrapper/python
+
+# test Ascon-128a implementation
+pushd wrapper/python
+
+pytest -k ascon_128a_kat --cache-clear -v -s
+rm LWC_AEAD_KAT_128_128.txt
+
+popd
+
+# --- --- ---- --- --- #
 
 # clean it up
 rm -rf pyascon
