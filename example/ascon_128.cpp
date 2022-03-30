92f218b2aa24b1c99332028d6b8a37ef470cea55
Anjan Roy <anjanroy@yandex.com>
2022-03-30 13:12:30 +0000

    ascon-128, 128a usage example

---
+++ example/ascon_128.cpp
@@ -0,0 +1,63 @@
+#include "ascon.hpp"
+#include <cassert>
+#include <iostream>
+
+int
+main(int argc, char** argv)
+{
+  constexpr const size_t text_len = 64;      // bytes
+  constexpr const size_t data_len = 32;      // bytes
+  constexpr const size_t enc_len = text_len; // bytes
+  constexpr const size_t dec_len = enc_len;  // bytes
+
+  // acquire resources
+  uint8_t* text = static_cast<uint8_t*>(malloc(text_len)); // plain text
+  uint8_t* data = static_cast<uint8_t*>(malloc(data_len)); // associated data
+  uint8_t* enc = static_cast<uint8_t*>(malloc(enc_len));   // ciphered data
+  uint8_t* dec = static_cast<uint8_t*>(malloc(dec_len));   // deciphered data
+
+  // prepare 128 -bit secret key
+  ascon::secret_key_t k = { { 1ul, 2ul } };
+  // prepare 128 -bit message nonce, don't repeat nonce for same secret key !
+  ascon::nonce_t n = { { 3ul, 4ul } };
+
+  // prepare associated data, it's never encrypted !
+#pragma unroll 8
+  for (size_t i = 0; i < data_len; i++) {
+    data[i] = static_cast<uint8_t>(i);
+  }
+
+  // prepare plain text, it'll be encrypted !
+#pragma unroll 8
+  for (size_t i = 0; i < text_len; i++) {
+    text[i] = static_cast<uint8_t>(i);
+  }
+
+  // using Ascon-128 for running encrypt -> decrypt cycle
+  using namespace ascon;
+  const tag_t t = encrypt_128(k, n, data, data_len, text, text_len, enc);
+  const bool f = decrypt_128(k, n, data, data_len, enc, enc_len, dec, t);
+
+  // verified decryption; it must be true !
+  assert(f);
+
+  const std::string text_ = ascon_utils::tohex(text, text_len);
+  const std::string enc_ = ascon_utils::tohex(enc, enc_len);
+  const std::string dec_ = ascon_utils::tohex(dec, dec_len);
+
+  // redundant check; if `f` is true, `dec` is good to consume !
+  assert(text_ == dec_);
+
+  std::cout << "Ascon-128 AEAD" << std::endl << std::endl;
+  std::cout << "Plain      text :\t" << text_ << std::endl;
+  std::cout << "Cipher     text :\t" << enc_ << std::endl;
+  std::cout << "Deciphered text :\t" << dec_ << std::endl;
+
+  // deallocate resources
+  free(text);
+  free(data);
+  free(enc);
+  free(dec);
+
+  return EXIT_SUCCESS;
+}
