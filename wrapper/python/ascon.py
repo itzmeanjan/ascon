0556ce436706439dcdc59f22e021fb6099752db7
Anjan Roy <anjanroy@yandex.com>
2022-04-07 12:04:46 +0000

    implemented python wrapper functions for invoking ascon-80pq encrypt/ decrypt routines which is compiled to a shared library object

--- wrapper/python/ascon.py
+++ wrapper/python/ascon.py
@@ -7,6 +7,7 @@
   implementation, executed on host CPU.
 
   Author: Anjan Roy <hello@itzmeanjan.in>
+
   Project: https://github.com/itzmeanjan/ascon
 '''
 
@@ -24,10 +25,18 @@ SO_LIB: ct.CDLL = ct.CDLL(SO_PATH)
 
 class secret_key_128_t(ct.Structure):
     '''
-    128 -bit Ascon secret key; see table 1 of Ascon specification 
+    128 -bit Ascon-{128, 128a} secret key; see table 1 of Ascon specification 
     https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/ascon-spec-final.pdf
     '''
     _fields_ = [('limbs', ct.c_uint64 * 2)]  # uint64_t[2]
+
+
+class secret_key_160_t(ct.Structure):
+    '''
+    160 -bit Ascon-80pq secret key; see last paragraph of section 2.2 of Ascon specification 
+    https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/ascon-spec-final.pdf
+    '''
+    _fields_ = [('limbs', ct.c_uint64 * 3)]  # uint64_t[3]
 
 
 class nonce_t(ct.Structure):
@@ -57,6 +66,7 @@ text_t = np.ctypeslib.ndpointer(dtype=np.uint8, ndim=1, flags='CONTIGUOUS')
 cipher_t = np.ctypeslib.ndpointer(dtype=np.uint8, ndim=1, flags='CONTIGUOUS')
 
 secret_key_128_tp = ct.POINTER(secret_key_128_t)
+secret_key_160_tp = ct.POINTER(secret_key_160_t)
 nonce_tp = ct.POINTER(nonce_t)
 tag_tp = ct.POINTER(tag_t)
 
@@ -144,7 +154,8 @@ def encrypt_128(key: bytes, nonce: bytes, data: np.ndarray, text: np.ndarray) ->
     nonce_ = nonce_t(limbs=(l0, l1))
     nonce_ = ct.byref(nonce_)
 
-    args = [secret_key_128_tp, nonce_tp, data_t, len_t, text_t, len_t, cipher_t]
+    args = [secret_key_128_tp, nonce_tp,
+            data_t, len_t, text_t, len_t, cipher_t]
 
     # set function return type
     SO_LIB.encrypt_128.restype = tag_t
@@ -266,7 +277,8 @@ def encrypt_128a(key: bytes, nonce: bytes, data: np.ndarray, text: np.ndarray) -
     nonce_ = nonce_t(limbs=(l0, l1))
     nonce_ = ct.byref(nonce_)
 
-    args = [secret_key_128_tp, nonce_tp, data_t, len_t, text_t, len_t, cipher_t]
+    args = [secret_key_128_tp, nonce_tp,
+            data_t, len_t, text_t, len_t, cipher_t]
 
     # set function return type
     SO_LIB.encrypt_128a.restype = tag_t
@@ -342,6 +354,131 @@ def decrypt_128a(key: bytes, nonce: bytes, data: np.ndarray, cipher: np.ndarray,
     # decrypt using Ascon-128a
     v = SO_LIB.decrypt_128a(key_, nonce_, data, d_len,
                             cipher, c_len, text, tag_)
+
+    assert v, 'tag doesn\'t match, failed to decrypt !'
+
+    # return decryption status, decrypted plain text
+    return v, text
+
+
+def encrypt_80pq(key: bytes, nonce: bytes, data: np.ndarray, text: np.ndarray) -> Tuple[np.ndarray, bytes]:
+    '''
+    Encrypts plain text using Ascon-80pq authenticated encryption algorithm, producing
+    encrypted text of length same as input plain text and 128 -bit authentication tag; see algorithm 1 in
+    Ascon specification https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/ascon-spec-final.pdf
+
+    Input :
+        - 160 -bit secret key ( as bytes )
+        - 128 -bit nonce ( as bytes )
+        - arbitrary length ( >= 0 ) associated data ( numpy ndarray of data type `uint8` )
+        - arbitrary length ( >= 0 ) plain text ( numpy ndarray of data type `uint8` )
+    Output:
+        - arbitrary length ( = len(text) ) encrypted data ( numpy ndarray of data type `uint8` )
+        - 128 -bit tag ( as bytes )
+    '''
+    u8 = np.uint8
+
+    assert u8().dtype == data.dtype, 'expected numpy ndarray[u8] as input'
+    assert u8().dtype == text.dtype, 'expected numpy ndarray[u8] as input'
+
+    d_len = data.size  # >= 0 bytes
+    t_len = text.size  # >= 0 bytes
+    cipher = np.empty(t_len, dtype=u8)  # allocate memory for keeping cipher
+
+    assert len(key) == 20  # 160 -bit secret key
+    assert len(nonce) == 16  # 128 -bit public message nonce
+
+    l0 = int.from_bytes(key[:8], 'big', signed=False)
+    l1 = int.from_bytes(key[8:16], 'big', signed=False)
+    l2 = int.from_bytes(key[16:], 'big', signed=False)
+
+    key_ = secret_key_160_t(limbs=(l0, l1, l2))
+    key_ = ct.byref(key_)
+
+    l0 = int.from_bytes(nonce[:8], 'big', signed=False)
+    l1 = int.from_bytes(nonce[8:], 'big', signed=False)
+
+    nonce_ = nonce_t(limbs=(l0, l1))
+    nonce_ = ct.byref(nonce_)
+
+    args = [secret_key_160_tp, nonce_tp,
+            data_t, len_t, text_t, len_t, cipher_t]
+
+    # set function return type
+    SO_LIB.encrypt_80pq.restype = tag_t
+    # set function signature
+    SO_LIB.encrypt_80pq.argtypes = args
+
+    # encrypt using Ascon-80pq
+    tag = SO_LIB.encrypt_80pq(key_, nonce_, data, d_len, text, t_len, cipher)
+
+    # converting tag to byte array
+    tag_ = tag.limbs[0].to_bytes(8, 'big') + tag.limbs[1].to_bytes(8, 'big')
+
+    # return cipher text, tag ( 128 -bit )
+    return cipher, tag_
+
+
+def decrypt_80pq(key: bytes, nonce: bytes, data: np.ndarray, cipher: np.ndarray, tag: bytes) -> Tuple[bool, np.ndarray]:
+    '''
+    Decrypts ciphered text using Ascon-80pq verified decryption algorithm, producing
+    plain text of length same as input ciphered text and boolean flag denoting
+    status of successful decryption; see algorithm 1 in Ascon specification
+    https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/ascon-spec-final.pdf
+
+    Input :
+        - 160 -bit secret key ( as bytes )
+        - 128 -bit nonce ( as bytes )
+        - arbitrary length ( >= 0 ) associated data ( numpy ndarray of data type `uint8` )
+        - arbitrary length ( >= 0 ) ciphered text ( numpy ndarray of data type `uint8` )
+        - 128 -bit tag ( as bytes )
+    Output:
+        - status of successful decryption
+        - arbitrary length ( = len(cipher) ) decrypted plain text ( numpy ndarray of data type `uint8` )
+    '''
+    u8 = np.uint8
+
+    assert u8().dtype == data.dtype, 'expected numpy ndarray[u8] as input'
+    assert u8().dtype == cipher.dtype, 'expected numpy ndarray[u8] as input'
+
+    d_len = data.size  # >= 0 bytes
+    c_len = cipher.size  # >= 0 bytes
+    text = np.empty(c_len, dtype=u8)  # allocate memory for keeping plain text
+
+    assert len(key) == 20  # 160 -bit secret key
+    assert len(nonce) == 16  # 128 -bit nonce
+    assert len(tag) == 16  # 128 -bit tag
+
+    l0 = int.from_bytes(key[:8], 'big', signed=False)
+    l1 = int.from_bytes(key[8:16], 'big', signed=False)
+    l2 = int.from_bytes(key[16:], 'big', signed=False)
+
+    key_ = secret_key_160_t(limbs=(l0, l1, l2))
+    key_ = ct.byref(key_)
+
+    l0 = int.from_bytes(nonce[:8], 'big', signed=False)
+    l1 = int.from_bytes(nonce[8:], 'big', signed=False)
+
+    nonce_ = nonce_t(limbs=(l0, l1))
+    nonce_ = ct.byref(nonce_)
+
+    l0 = int.from_bytes(tag[:8], 'big', signed=False)
+    l1 = int.from_bytes(tag[8:], 'big', signed=False)
+
+    tag_ = tag_t(limbs=(l0, l1))
+    tag_ = ct.byref(tag_)
+
+    args = [secret_key_160_tp, nonce_tp, data_t,
+            len_t, cipher_t, len_t, text_t, tag_tp]
+
+    # set function return type
+    SO_LIB.decrypt_80pq.restype = ct.c_bool
+    # set function signature
+    SO_LIB.decrypt_80pq.argtypes = args
+
+    # decrypt using Ascon-80pq
+    v = SO_LIB.decrypt_80pq(key_, nonce_, data, d_len,
+                           cipher, c_len, text, tag_)
 
     assert v, 'tag doesn\'t match, failed to decrypt !'
 
