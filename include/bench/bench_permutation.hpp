255b0205392cd3dedb61ddf9455fa39655c54b81
Anjan Roy <hello@itzmeanjan.in>
2022-12-28 11:26:53 +0400

    benchmark ascon permutation on target CPU
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

---
+++ include/bench/bench_permutation.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include "permutation.hpp"
+#include "utils.hpp"
+#include <benchmark/benchmark.h>
+
+// Benchmark Ascon Light Weight Cryptography Implementation
+namespace bench_ascon {
+
+// Benchmarking Ascon permutation routine, while applying `ROUNDS` -many
+// permutation round
+template<const size_t ROUNDS>
+static void
+ascon_permutation(benchmark::State& state)
+{
+  static_assert(ROUNDS <= 12);
+
+  uint64_t st[5];
+  ascon_utils::random_data(st, 5);
+
+  for (auto _ : state) {
+    ascon_perm::permute<ROUNDS>(st);
+
+    benchmark::DoNotOptimize(st);
+    benchmark::ClobberMemory();
+  }
+
+  constexpr size_t len = sizeof(st);
+  state.SetBytesProcessed(static_cast<int64_t>(len * state.iterations()));
+}
+
+}
