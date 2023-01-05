5d014513c864e4558143d67726ab7e83b16485fc
Anjan Roy <hello@itzmeanjan.in>
2023-01-05 22:57:51 +0400

    refactor absorb/ squeeze routines
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/hash_utils.hpp
+++ include/hash_utils.hpp
@@ -34,27 +34,28 @@ template<const size_t b>
 static inline void
 absorb(uint64_t* const __restrict state,
        const uint8_t* const __restrict msg,
-       const size_t msg_len // in terms of bytes, can be >= 0
+       const size_t mlen // in terms of bytes, can be >= 0
 )
 {
-  // these many 0 -bits to be appended to input message
-  const size_t tmp = (msg_len << 3) & 63ul;            // bits
-  const size_t zero_pad_len = 63ul - tmp;              // bits
-  const size_t pad_byte_len = (zero_pad_len + 1) >> 3; // bytes
+  const size_t mbits = mlen << 3;
+  const size_t rm_bits = mbits & 63ul;
+  const size_t zero_pad_bits = 63ul - rm_bits;
+  constexpr size_t one_pad_bit = 1ul;
+  const size_t pad_bytes = (one_pad_bit + zero_pad_bits) >> 3;
 
-  const uint8_t* msg_ = msg + msg_len - (8ul - pad_byte_len); // slice out
-  const uint64_t last_msg_blk = ascon_utils::pad_data(msg_, pad_byte_len);
+  const size_t till = mlen - (rm_bits >> 3);
+  size_t off = 0ul;
 
-  const size_t msg_blk_cnt = ((msg_len + pad_byte_len) << 3) >> 6;
-
-  for (size_t i = 0; i < msg_blk_cnt - 1; i++) {
-    const uint64_t msg_blk = ascon_utils::from_be_bytes(msg + (i << 3));
-
-    state[0] ^= msg_blk;
+  while (off < till) {
+    const auto word = ascon_utils::from_be_bytes(msg + off);
+    state[0] ^= word;
     ascon_perm::permute<b>(state);
+
+    off += 8ul;
   }
 
-  state[0] ^= last_msg_blk;
+  const auto word = ascon_utils::pad_data(msg + off, pad_bytes);
+  state[0] ^= word;
 }
 
 // Extract out four 64 -bit blocks from hash state, producing total 256 -bit
@@ -69,10 +70,20 @@ squeeze(uint64_t* const __restrict state, uint8_t* const __restrict digest)
 {
   ascon_perm::permute<a>(state);
 
-  for (size_t i = 0; i < 4; i++) {
-    const uint64_t block = state[0];
-    ascon_utils::to_be_bytes(block, digest + (i << 3));
+#if defined __clang__
+  // Following
+  // https://clang.llvm.org/docs/LanguageExtensions.html#extensions-for-loop-hint-optimizations
 
+#pragma clang loop unroll(enable)
+#pragma clang loop vectorize(enable)
+#elif defined __GNUG__
+  // Following
+  // https://gcc.gnu.org/onlinedocs/gcc/Loop-Specific-Pragmas.html#Loop-Specific-Pragmas
+
+#pragma GCC unroll 4
+#endif
+  for (size_t i = 0; i < 4; i++) {
+    ascon_utils::to_be_bytes(state[0], digest + i * 8);
     ascon_perm::permute<b>(state);
   }
 }
