3fa7625b60a3e7cd29b54cd2b886fa2f0a4acdce
Anjan Roy <hello@itzmeanjan.in>
2023-02-14 08:34:58 +0400

    make ascon permutation `constexpr`
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/permutation.hpp
+++ include/permutation.hpp
@@ -11,16 +11,16 @@ constexpr size_t ROUNDS = 12;
 
 // Ascon  permutation round constants; taken from table 4 in Ascon specification
 // https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/ascon-spec-final.pdf
-constexpr uint64_t RC[ROUNDS] = { 0x00000000000000f0ul, 0x00000000000000e1ul,
-                                  0x00000000000000d2ul, 0x00000000000000c3ul,
-                                  0x00000000000000b4ul, 0x00000000000000a5ul,
-                                  0x0000000000000096ul, 0x0000000000000087ul,
-                                  0x0000000000000078ul, 0x0000000000000069ul,
-                                  0x000000000000005aul, 0x000000000000004bul };
+constexpr uint64_t RC[ROUNDS]{ 0x00000000000000f0ul, 0x00000000000000e1ul,
+                               0x00000000000000d2ul, 0x00000000000000c3ul,
+                               0x00000000000000b4ul, 0x00000000000000a5ul,
+                               0x0000000000000096ul, 0x0000000000000087ul,
+                               0x0000000000000078ul, 0x0000000000000069ul,
+                               0x000000000000005aul, 0x000000000000004bul };
 
 // Addition of constants step; see section 2.6.1 of Ascon specification
 // https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/ascon-spec-final.pdf
-static inline void
+static inline constexpr void
 p_c(uint64_t* const state, const size_t r_idx)
 {
   state[2] ^= RC[r_idx];
@@ -29,7 +29,7 @@ p_c(uint64_t* const state, const size_t r_idx)
 // Substitution layer i.e. 5 -bit S-box S(x) applied on Ascon state; taken from
 // figure 5 in Ascon specification
 // https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/ascon-spec-final.pdf
-static inline void
+static inline constexpr void
 p_s(uint64_t* const state)
 {
   state[0] ^= state[4];
@@ -56,7 +56,7 @@ p_s(uint64_t* const state)
 
 // Linear diffusion layer; taken from figure 4.b in Ascon specification
 // https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/ascon-spec-final.pdf
-static inline void
+static inline constexpr void
 p_l(uint64_t* const state)
 {
   using namespace std;
@@ -71,7 +71,7 @@ p_l(uint64_t* const state)
 // Single round of Ascon permutation; taken from section 2.6 of Ascon
 // specification
 // https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/ascon-spec-final.pdf
-static inline void
+static inline constexpr void
 round(uint64_t* const state, const size_t r_idx)
 {
   p_c(state, r_idx);
@@ -79,20 +79,13 @@ round(uint64_t* const state, const size_t r_idx)
   p_l(state);
 }
 
-// Compile time check to ensure template argument of `permute(...)` is <= 12
-consteval bool
-check_lte12(const size_t a)
-{
-  return a <= 12;
-}
-
 // Sequentially apply Ascon permutation round for R -many times | R <= 12;
 // taken from section 2.6 of Ascon specification
 // https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/ascon-spec-final.pdf
 template<const size_t R>
-static inline void
+static inline constexpr void
 permute(uint64_t* const state)
-  requires(check_lte12(R))
+  requires(R <= ROUNDS)
 {
   constexpr size_t BEG = ROUNDS - R;
 
