e5d129eb28f8a8fb2b15528499b6a52e52dc11d5
Anjan Roy <hello@itzmeanjan.in>
2023-02-17 10:54:52 +0400

    update examples demonstrating usage of ascon-{hash, hasha} API
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- example/ascon_hash.cpp
+++ example/ascon_hash.cpp
@@ -1,4 +1,4 @@
-#include "hash.hpp"
+#include "ascon_hash.hpp"
 #include <iostream>
 
 // Compile with
@@ -11,10 +11,16 @@ main()
 
   // acquire resources
   uint8_t* msg = static_cast<uint8_t*>(malloc(msg_len)); // input
-  uint8_t* out = static_cast<uint8_t*>(malloc(ascon::DIGEST_LEN));
+  uint8_t* out = static_cast<uint8_t*>(malloc(ascon::ASCON_HASH_DIGEST_LEN));
 
   ascon_utils::random_data(msg, msg_len);
-  ascon::hash(msg, msg_len, out);
+
+  // Opting for using incremental hashing API by passing explicit value `true`
+  // to template parameter.
+  ascon::ascon_hash<true> hasher;
+  hasher.absorb(msg, msg_len);
+  hasher.finalize();
+  hasher.digest(out);
 
   std::cout << "Ascon Hash\n\n";
   std::cout << "Message :\t" << ascon_utils::to_hex(msg, msg_len) << "\n";
