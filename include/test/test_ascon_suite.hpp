4fd93a05c2e17ad1726069a221cd89317c1f1a86
Anjan Roy <hello@itzmeanjan.in>
2023-06-20 19:22:54 +0400

    rename file
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/test/test_ascon_suite.hpp
+++ include/test/test_ascon_suite.hpp
@@ -5,6 +5,6 @@
 #include "test_ascon80pq_aead.hpp"
 #include "test_ascon_hash.hpp"
 #include "test_ascon_hasha.hpp"
+#include "test_ascon_perm.hpp"
 #include "test_ascon_xof.hpp"
 #include "test_ascon_xofa.hpp"
-#include "test_permutation.hpp"
