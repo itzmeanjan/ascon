0bc91343f5d380085bba2e02e660209756f89919
Anjan Roy <hello@itzmeanjan.in>
2023-06-27 12:50:53 +0400

    benchmark Ascon-HashA for variable length input
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

---
+++ include/hash/bench_ascon_hasha.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include "ascon_hasha.hpp"
+#include <benchmark/benchmark.h>
+#include <vector>
+
+// Benchmark Ascon Light Weight Cryptography Implementation
+namespace bench_ascon {
+
+// Benchmark Ascon-HashA with variable length input.
+inline void
+ascon_hasha(benchmark::State& state)
+{
+  const size_t mlen = static_cast<size_t>(state.range(0));
+
+  std::vector<uint8_t> msg(mlen);
+  std::vector<uint8_t> dig(ascon_hasha::DIGEST_LEN);
+
+  ascon_utils::random_data(msg.data(), msg.size());
+
+  for (auto _ : state) {
+    ascon_hasha::ascon_hasha hasher;
+
+    hasher.absorb(msg.data(), msg.size());
+    hasher.finalize();
+    hasher.digest(dig.data());
+
+    benchmark::DoNotOptimize(hasher);
+    benchmark::DoNotOptimize(msg);
+    benchmark::DoNotOptimize(dig);
+    benchmark::ClobberMemory();
+  }
+
+  state.SetBytesProcessed((mlen + ascon_hasha::DIGEST_LEN) *
+                          state.iterations());
+}
+
+}
