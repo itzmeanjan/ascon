a7c7fc83a70a223069328231bb7cf4a4e04d7816
Anjan Roy <hello@itzmeanjan.in>
2023-06-27 09:45:55 +0400

    move Ascon-128 AEAD tests (both property based and KAT tests) to different directory
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/aead/test_ascon128_aead.hpp
+++ include/aead/test_ascon128_aead.hpp
@@ -1,9 +1,5 @@
 #pragma once
-#include "aead.hpp"
-#include "auth_enc.hpp"
-#include "consts.hpp"
-#include "utils.hpp"
-#include "verf_dec.hpp"
+#include "ascon128.hpp"
 #include <cassert>
 #include <cstdint>
 #include <fstream>
@@ -14,41 +10,44 @@ namespace ascon_test {
 using namespace std::literals;
 
 // Test functional correctness of Ascon-128 authenticated encryption and
-// verified decryption implementation for different input sizes
+// verified decryption implementation for different input sizes.
 inline void
 ascon128_aead(const size_t dlen, // bytes; >= 0
               const size_t ctlen // bytes; >= 0
 )
 {
-  auto key = static_cast<uint8_t*>(std::malloc(ascon::ASCON128_KEY_LEN));
-  auto nonce = static_cast<uint8_t*>(std::malloc(ascon::ASCON128_NONCE_LEN));
-  auto tag = static_cast<uint8_t*>(std::malloc(ascon::ASCON128_TAG_LEN));
-  auto data = static_cast<uint8_t*>(std::malloc(dlen));
-  auto text = static_cast<uint8_t*>(std::malloc(ctlen));
-  auto enc = static_cast<uint8_t*>(std::malloc(ctlen));
-  auto dec = static_cast<uint8_t*>(std::malloc(ctlen));
+  std::vector<uint8_t> key(ascon128_aead::KEY_LEN);
+  std::vector<uint8_t> nonce(ascon128_aead::NONCE_LEN);
+  std::vector<uint8_t> tag(ascon128_aead::TAG_LEN);
+  std::vector<uint8_t> data(dlen);
+  std::vector<uint8_t> text(ctlen);
+  std::vector<uint8_t> enc(ctlen);
+  std::vector<uint8_t> dec(ctlen);
 
-  ascon_utils::random_data(key, ascon::ASCON128_KEY_LEN);
-  ascon_utils::random_data(nonce, ascon::ASCON128_NONCE_LEN);
-  ascon_utils::random_data(data, dlen);
-  ascon_utils::random_data(text, ctlen);
+  ascon_utils::random_data(key.data(), key.size());
+  ascon_utils::random_data(nonce.data(), nonce.size());
+  ascon_utils::random_data(data.data(), data.size());
+  ascon_utils::random_data(text.data(), text.size());
 
-  ascon::encrypt_128(key, nonce, data, dlen, text, ctlen, enc, tag);
-  bool v = ascon::decrypt_128(key, nonce, data, dlen, enc, ctlen, dec, tag);
+  ascon128_aead::encrypt(key.data(),
+                         nonce.data(),
+                         data.data(),
+                         data.size(),
+                         text.data(),
+                         text.size(),
+                         enc.data(),
+                         tag.data());
+  bool flag = ascon128_aead::decrypt(key.data(),
+                                     nonce.data(),
+                                     data.data(),
+                                     data.size(),
+                                     enc.data(),
+                                     enc.size(),
+                                     dec.data(),
+                                     tag.data());
 
-  // ensures that text has been verifiably decrypted !
-  assert(v);
-  for (size_t i = 0; i < ctlen; i++) {
-    assert(text[i] == dec[i]);
-  }
-
-  std::free(key);
-  std::free(nonce);
-  std::free(data);
-  std::free(text);
-  std::free(enc);
-  std::free(dec);
-  std::free(tag);
+  assert(flag);
+  assert(std::ranges::equal(text, dec));
 }
 
 // Ensure that Ascon-128 AEAD implementation conforms to the specification,
@@ -94,25 +93,25 @@ ascon128_aead_kat()
       auto ct = ascon_utils::from_hex(ct2);
 
       std::vector<uint8_t> ctxt(pt.size());
-      std::vector<uint8_t> tag(ascon::ASCON128_TAG_LEN);
+      std::vector<uint8_t> tag(ascon128_aead::TAG_LEN);
       std::vector<uint8_t> ptxt(ctxt.size());
 
-      ascon::encrypt_128(key.data(),
-                         nonce.data(),
-                         ad.data(),
-                         ad.size(),
-                         pt.data(),
-                         pt.size(),
-                         ctxt.data(),
-                         tag.data());
-      bool flag = ascon::decrypt_128(key.data(),
-                                     nonce.data(),
-                                     ad.data(),
-                                     ad.size(),
-                                     ctxt.data(),
-                                     ctxt.size(),
-                                     ptxt.data(),
-                                     tag.data());
+      ascon128_aead::encrypt(key.data(),
+                             nonce.data(),
+                             ad.data(),
+                             ad.size(),
+                             pt.data(),
+                             pt.size(),
+                             ctxt.data(),
+                             tag.data());
+      bool flag = ascon128_aead::decrypt(key.data(),
+                                         nonce.data(),
+                                         ad.data(),
+                                         ad.size(),
+                                         ctxt.data(),
+                                         ctxt.size(),
+                                         ptxt.data(),
+                                         tag.data());
 
       assert(flag);
       assert(ascon_utils::ct_eq_byte_array(ctxt.data(), ct.data(), pt.size()));
