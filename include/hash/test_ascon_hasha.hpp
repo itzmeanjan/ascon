4253a03db44853227b9666ba01aa5db2018a605c
Anjan Roy <hello@itzmeanjan.in>
2023-06-27 10:04:35 +0400

    move Ascon-HashA tests (both property based and KAT tests) to different directory
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/hash/test_ascon_hasha.hpp
+++ include/hash/test_ascon_hasha.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include "ascon_hasha.hpp"
-#include <algorithm>
 #include <cassert>
 #include <fstream>
 
@@ -9,51 +8,43 @@ namespace ascon_test {
 
 using namespace std::literals;
 
-// Test if both oneshot and incremental hashing API of Ascon-HashA produces same
-// result for same input message.
+// Test if both oneshot and incremental hashing of same message, using
+// Ascon-HashA, produces same digest or not.
 inline void
 test_ascon_hasha(const size_t mlen)
 {
-  uint8_t digest_oneshot[ascon::ASCON_HASHA_DIGEST_LEN];
-  uint8_t digest_incremental[ascon::ASCON_HASHA_DIGEST_LEN];
+  std::vector<uint8_t> digest_oneshot(ascon_hasha::DIGEST_LEN);
+  std::vector<uint8_t> digest_incremental(ascon_hasha::DIGEST_LEN);
+  std::vector<uint8_t> msg(mlen);
 
-  auto msg = static_cast<uint8_t*>(std::malloc(mlen));
-  ascon_utils::random_data(msg, mlen);
+  ascon_utils::random_data(msg.data(), msg.size());
 
   // oneshot hashing
   {
-    ascon::ascon_hasha hasher;
+    ascon_hasha::ascon_hasha hasher;
 
-    hasher.hash(msg, mlen);
-    hasher.digest(digest_oneshot);
+    hasher.hash(msg.data(), msg.size());
+    hasher.digest(digest_oneshot.data());
   }
 
   // incremental hashing
   {
-    ascon::ascon_hasha<true> hasher;
+    ascon_hasha::ascon_hasha<true> hasher;
 
     size_t off = 0;
     while (off < mlen) {
       // because we don't want to be stuck in an infinite loop if msg[off] = 0
       auto elen = std::min<size_t>(std::max<uint8_t>(msg[off], 1), mlen - off);
 
-      hasher.absorb(msg + off, elen);
+      hasher.absorb(msg.data() + off, elen);
       off += elen;
     }
 
     hasher.finalize();
-    hasher.digest(digest_incremental);
+    hasher.digest(digest_incremental.data());
   }
 
-  // compare both 32 -bytes digests
-  bool flg = false;
-  for (size_t i = 0; i < ascon::ASCON_HASHA_DIGEST_LEN; i++) {
-    flg |= static_cast<bool>(digest_oneshot[i] ^ digest_incremental[i]);
-  }
-
-  std::free(msg);
-
-  assert(!flg);
+  assert(std::ranges::equal(digest_oneshot, digest_incremental));
 }
 
 // Ensure that this Ascon-HashA implementation is conformant to the
@@ -83,12 +74,14 @@ test_ascon_hasha_kat()
       auto msg = ascon_utils::from_hex(msg2);
       auto md = ascon_utils::from_hex(md2);
 
-      std::vector<uint8_t> digest(ascon::ASCON_HASHA_DIGEST_LEN);
+      std::vector<uint8_t> digest(ascon_hasha::DIGEST_LEN);
 
-      ascon::ascon_hasha hasher;
+      {
+        ascon_hasha::ascon_hasha hasher;
 
-      hasher.hash(msg.data(), msg.size());
-      hasher.digest(digest.data());
+        hasher.hash(msg.data(), msg.size());
+        hasher.digest(digest.data());
+      }
 
       assert(std::ranges::equal(digest, md));
 
