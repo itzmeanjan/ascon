47bb4663a8d08ac1d358eab7d5d9245615cdbcf7
Anjan Roy <hello@itzmeanjan.in>
2023-06-27 12:24:25 +0400

    relocate Ascon permutation benchmark routine
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/bench_permutation.hpp
+++ include/bench_permutation.hpp
@@ -7,24 +7,23 @@
 namespace bench_ascon {
 
 // Benchmarking Ascon permutation routine, while applying `ROUNDS` -many
-// permutation round
+// permutation round.
 template<const size_t ROUNDS>
 void
-permutation(benchmark::State& state)
-  requires(ROUNDS <= ascon_perm::ROUNDS)
+ascon_permutation(benchmark::State& state)
+  requires(ROUNDS <= ascon_permutation::MAX_ROUNDS)
 {
   uint64_t st[5];
   ascon_utils::random_data(st, 5);
 
   for (auto _ : state) {
-    ascon_perm::permute<ROUNDS>(st);
+    ascon_permutation::permute<ROUNDS>(st);
 
     benchmark::DoNotOptimize(st);
     benchmark::ClobberMemory();
   }
 
-  constexpr size_t len = sizeof(st);
-  state.SetBytesProcessed(static_cast<int64_t>(len * state.iterations()));
+  state.SetBytesProcessed(sizeof(st) * state.iterations());
 }
 
 }
