332e11f8ddbc13724af40301523778dffb2a018a
Anjan Roy <hello@itzmeanjan.in>
2023-06-27 12:10:12 +0400

    move Ascon-XofA tests (both property based and KAT tests) to different directory
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/xof/test_ascon_xofa.hpp
+++ include/xof/test_ascon_xofa.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include "ascon_xofa.hpp"
-#include <algorithm>
 #include <cassert>
 #include <fstream>
 
@@ -9,68 +8,55 @@ namespace ascon_test {
 
 using namespace std::literals;
 
-// Test if both oneshot and incremental hashing API of Ascon-XofA produces same
-// result for same input message.
+// Test if both oneshot and incremental hashing of same message, using
+// Ascon-XofA, produces same output bytes or not.
 inline void
 test_ascon_xofa(const size_t mlen, const size_t dlen)
 {
-  auto msg = static_cast<uint8_t*>(std::malloc(mlen));
-  auto dig_oneshot = static_cast<uint8_t*>(std::malloc(dlen));
-  auto dig_incremental = static_cast<uint8_t*>(std::malloc(dlen));
+  std::vector<uint8_t> msg(mlen);
+  std::vector<uint8_t> dig_oneshot(dlen);
+  std::vector<uint8_t> dig_incremental(dlen);
 
-  ascon_utils::random_data(msg, mlen);
+  ascon_utils::random_data(msg.data(), msg.size());
 
   // oneshot hashing
   {
-    ascon::ascon_xofa hasher;
+    ascon_xofa::ascon_xofa hasher;
 
-    // absorb all message bytes at once
-    hasher.hash(msg, mlen);
-    // squeeze all digest bytes at once
-    hasher.read(dig_oneshot, dlen);
+    hasher.absorb(msg.data(), msg.size());
+    hasher.finalize();
+    hasher.read(dig_oneshot.data(), dig_oneshot.size());
   }
 
   // incremental hashing
   {
-    ascon::ascon_xofa<true> hasher;
+    ascon_xofa::ascon_xofa hasher;
 
-    // absorb message bytes in many iterations
     size_t off = 0;
     while (off < mlen) {
       // because we don't want to be stuck in an infinite loop if msg[off] = 0
       auto elen = std::min<size_t>(std::max<uint8_t>(msg[off], 1), mlen - off);
 
-      hasher.absorb(msg + off, elen);
+      hasher.absorb(msg.data() + off, elen);
       off += elen;
     }
 
-    // finalize absorption of message bytes
     hasher.finalize();
 
     // squeeze message bytes in many iterations
     off = 0;
     while (off < dlen) {
-      hasher.read(dig_incremental + off, 1);
+      hasher.read(dig_incremental.data() + off, 1);
 
       auto elen = std::min<size_t>(dig_incremental[off], dlen - (off + 1));
 
       off += 1;
-      hasher.read(dig_incremental + off, elen);
+      hasher.read(dig_incremental.data() + off, elen);
       off += elen;
     }
   }
 
-  // ensure that both techniques produce same digest bytes !
-  bool flg = false;
-  for (size_t i = 0; i < dlen; i++) {
-    flg |= static_cast<bool>(dig_oneshot[i] ^ dig_incremental[i]);
-  }
-
-  std::free(msg);
-  std::free(dig_oneshot);
-  std::free(dig_incremental);
-
-  assert(!flg);
+  assert(std::ranges::equal(dig_oneshot, dig_incremental));
 }
 
 // Ensure that this Ascon-XofA implementation is conformant to the
@@ -102,9 +88,10 @@ test_ascon_xofa_kat()
 
       std::vector<uint8_t> digest(md.size());
 
-      ascon::ascon_xofa hasher;
+      ascon_xofa::ascon_xofa hasher;
 
-      hasher.hash(msg.data(), msg.size());
+      hasher.absorb(msg.data(), msg.size());
+      hasher.finalize();
       hasher.read(digest.data(), digest.size());
 
       assert(std::ranges::equal(digest, md));
