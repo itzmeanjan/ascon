5966767028f636688ee9734c6b2ccc77cfc85228
Anjan Roy <hello@itzmeanjan.in>
2023-07-26 18:52:45 +0400

    update Ascon-HashA implementation to conform to new API guideline
    Compute 320 -bit initial permutation state during compile-time from now on.
    
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/hashing/ascon_hasha.hpp
+++ include/hashing/ascon_hasha.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "ascon_perm.hpp"
 #include "sponge.hpp"
 
 // Ascon-Hasha
@@ -14,7 +15,13 @@ constexpr size_t ROUNDS_A = 12;
 constexpr size_t ROUNDS_B = 8;
 
 // Ascon HashA Digest Byte Length
-constexpr size_t DIGEST_LEN = 32;
+constexpr size_t DIGEST_LEN = sizeof(ascon_perm::ascon_perm_t) - (RATE / 8);
+
+// 64 -bit initialization value, obtained from section 2.5.1 of spec.
+constexpr uint64_t IV = 0x00400c0400000100ul;
+
+// Compile-time computed initial permutation state used in Ascon-HashA.
+constexpr auto INIT_PERM_STATE = sponge::compute_init_state(IV);
 
 // Ascon-HashA, supporting incremental hashing.
 //
@@ -23,18 +30,14 @@ constexpr size_t DIGEST_LEN = 32;
 struct ascon_hasha
 {
 private:
-  uint64_t state[5]{ 0x01470194fc6528a6,
-                     0x738ec38ac0adffa7,
-                     0x2ec8e3296c76384c,
-                     0xd6f6a54d7f52377d,
-                     0xa13c42a223be8d87 };
+  ascon_perm::ascon_perm_t state = INIT_PERM_STATE;
   size_t offset = 0;
   alignas(4) bool absorbed = false;
   alignas(4) bool squeezed = false;
 
 public:
-  // Initialization values taken from section 2.5.1 of Ascon spec.
-  constexpr ascon_hasha() = default;
+  // Constructor
+  constexpr inline ascon_hasha() = default;
 
   // Given N -bytes input message, this routine consumes those into
   // Ascon permutation state.
@@ -42,14 +45,10 @@ public:
   // Note, this routine can be called arbitrary number of times, each time with
   // arbitrary bytes of input message, until Ascon permutation state is
   // finalized ( by calling routine with similar name ).
-  //
-  // This function is only enabled, when you decide to use Ascon-HashA in
-  // incremental hashing mode ( compile-time decision ). By default one uses
-  // Ascon-HashA API in oneshot hashing mode.
-  inline void absorb(const uint8_t* const msg, const size_t mlen)
+  inline void absorb(std::span<const uint8_t> msg)
   {
     if (!absorbed) {
-      sponge::absorb<ROUNDS_B, RATE>(state, &offset, msg, mlen);
+      sponge::absorb<ROUNDS_B, RATE>(state, offset, msg);
     }
   }
 
@@ -61,31 +60,36 @@ public:
   // same Ascon-HashA object, doesn't do anything. After finalization, one would
   // like to read 32 -bytes of digest by squeezing sponge, which is done by
   // calling digest() function only once.
-  //
-  // This function is only enabled, when you decide to use Ascon-HashA in
-  // incremental hashing mode ( compile-time decision ). By default one uses
-  // Ascon-HashA API in oneshot hashing mode.
   inline void finalize()
   {
     if (!absorbed) {
-      sponge::finalize<ROUNDS_A, RATE>(state, &offset);
+      sponge::finalize<ROUNDS_A, RATE>(state, offset);
       absorbed = true;
     }
   }
 
-  // Given that N -bytes message is consumed into Ascon permutation state either
-  // using single-shot hashing API or incremental hashing API ( compile-time
-  // decision ), this routine can be used for squeezing out 32 -bytes message
-  // digest. Once squeezed, calling this function again and again doesn't have
-  // any effect.
-  inline void digest(uint8_t* const out)
+  // Given that N -bytes message is consumed into Ascon permutation state, this routine
+  // can be used for squeezing out 32 -bytes message digest. Once squeezed, calling this
+  // function again and again doesn't have any effect. You may continue using same
+  // hasher object for another round of absorb->finalize->digest flow, by explicitly
+  // calling reset() function.
+  inline void digest(std::span<uint8_t, DIGEST_LEN> out)
   {
     if (absorbed && !squeezed) {
       size_t readable = RATE / 8;
-      sponge::squeeze<ROUNDS_B, RATE>(state, &readable, out, DIGEST_LEN);
+      sponge::squeeze<ROUNDS_B, RATE>(state, readable, out);
 
       squeezed = true;
     }
+  }
+
+  // Resets internal state of Ascon-HashA object, making it ready for yet another
+  // absorb->finalize->digest round. You must explicitly call this function if you're
+  // interested in reusing the same object for absorbing new messages.
+  inline void reset()
+  {
+    std::memset(this, 0x00, sizeof(*this));
+    this->state = INIT_PERM_STATE;
   }
 };
 
