62f185b26490d8e62189d0d25f2f38d5c62d4456
Anjan Roy <hello@itzmeanjan.in>
2023-07-27 15:25:39 +0400

    add/ update tests for ensuring functional correctness and conformance of Ascon-Hash
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- tests/test_aead_common.hpp
+++ tests/test_aead_common.hpp
@@ -2,10 +2,14 @@
 #include <cstddef>
 #include <cstdint>
 
-// Min. and max. byte length of associated data
+// Min. and max. byte length of associated data.
 constexpr size_t MIN_AD_LEN = 0;
 constexpr size_t MAX_AD_LEN = 64;
 
-// Min. and max. byte length of plain/ cipher text
+// Min. and max. byte length of plain/ cipher text.
 constexpr size_t MIN_CT_LEN = 0;
 constexpr size_t MAX_CT_LEN = 128;
+
+// Min. and max. byte length of message to be absorbed into sponge.
+constexpr size_t MIN_MSG_LEN = 0;
+constexpr size_t MAX_MSG_LEN = 1024;
