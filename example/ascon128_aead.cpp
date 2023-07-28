4b8d32c8bef20372a89d97e818b8f8ab2c6fde38
Anjan Roy <hello@itzmeanjan.in>
2023-07-28 18:12:38 +0400

    update all API usage examples to use new std::span based interface
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- example/ascon128_aead.cpp
+++ example/ascon128_aead.cpp
@@ -12,41 +12,40 @@ main()
   constexpr size_t ctlen = 64; // bytes
   constexpr size_t dlen = 32;  // bytes
 
-  // acquire resources
-  uint8_t* key = static_cast<uint8_t*>(malloc(ascon128_aead::KEY_LEN));
-  uint8_t* nonce = static_cast<uint8_t*>(malloc(ascon128_aead::NONCE_LEN));
-  uint8_t* tag = static_cast<uint8_t*>(malloc(ascon128_aead::TAG_LEN));
-  uint8_t* data = static_cast<uint8_t*>(malloc(dlen));  // associated data
-  uint8_t* text = static_cast<uint8_t*>(malloc(ctlen)); // plain text
-  uint8_t* enc = static_cast<uint8_t*>(malloc(ctlen));  // ciphered text
-  uint8_t* dec = static_cast<uint8_t*>(malloc(ctlen));  // deciphered text
+  std::vector<uint8_t> key(ascon128_aead::KEY_LEN);
+  std::vector<uint8_t> nonce(ascon128_aead::NONCE_LEN);
+  std::vector<uint8_t> tag(ascon128_aead::TAG_LEN);
+  std::vector<uint8_t> data(dlen);
+  std::vector<uint8_t> text(ctlen);
+  std::vector<uint8_t> enc(ctlen);
+  std::vector<uint8_t> dec(ctlen);
 
-  ascon_utils::random_data(key, ascon128_aead::KEY_LEN);
-  ascon_utils::random_data(nonce, ascon128_aead::NONCE_LEN);
-  ascon_utils::random_data(text, ctlen);
-  ascon_utils::random_data(data, dlen);
+  auto _key = std::span<uint8_t, ascon128_aead::KEY_LEN>(key);
+  auto _nonce = std::span<uint8_t, ascon128_aead::NONCE_LEN>(nonce);
+  auto _tag = std::span<uint8_t, ascon128_aead::TAG_LEN>(tag);
+  auto _data = std::span(data);
+  auto _text = std::span(text);
+  auto _enc = std::span(enc);
+  auto _dec = std::span(dec);
 
-  ascon128_aead::encrypt(key, nonce, data, dlen, text, ctlen, enc, tag);
-  bool f = ascon128_aead::decrypt(key, nonce, data, dlen, enc, ctlen, dec, tag);
+  ascon_utils::random_data<uint8_t>(_key);
+  ascon_utils::random_data<uint8_t>(_nonce);
+  ascon_utils::random_data(_text);
+  ascon_utils::random_data(_data);
+
+  ascon128_aead::encrypt(_key, _nonce, _data, _text, _enc, _tag);
+  bool f = ascon128_aead::decrypt(_key, _nonce, _data, _enc, _dec, _tag);
 
   assert(f);
+  assert(std::ranges::equal(_text, _dec));
 
   std::cout << "Ascon-128 AEAD\n\n";
-  std::cout << "Key       :\t" << ascon_utils::to_hex(key, 16) << "\n";
-  std::cout << "Nonce     :\t" << ascon_utils::to_hex(nonce, 16) << "\n";
-  std::cout << "Data      :\t" << ascon_utils::to_hex(data, dlen) << "\n";
-  std::cout << "Text      :\t" << ascon_utils::to_hex(text, ctlen) << "\n";
-  std::cout << "Encrypted :\t" << ascon_utils::to_hex(enc, ctlen) << "\n";
-  std::cout << "Decrypted :\t" << ascon_utils::to_hex(dec, ctlen) << "\n";
-
-  // deallocate resources
-  free(key);
-  free(nonce);
-  free(tag);
-  free(data);
-  free(text);
-  free(enc);
-  free(dec);
+  std::cout << "Key       :\t" << ascon_utils::to_hex(_key) << "\n";
+  std::cout << "Nonce     :\t" << ascon_utils::to_hex(_nonce) << "\n";
+  std::cout << "Data      :\t" << ascon_utils::to_hex(_data) << "\n";
+  std::cout << "Text      :\t" << ascon_utils::to_hex(_text) << "\n";
+  std::cout << "Encrypted :\t" << ascon_utils::to_hex(_enc) << "\n";
+  std::cout << "Decrypted :\t" << ascon_utils::to_hex(_dec) << "\n";
 
   return EXIT_SUCCESS;
 }
--- example/ascon_xof.cpp
+++ example/ascon_xof.cpp
@@ -11,28 +11,22 @@ main()
   constexpr size_t msg_len = 64; // bytes
   constexpr size_t dig_len = 64; // bytes
 
-  // acquire resources
-  uint8_t* msg = static_cast<uint8_t*>(malloc(msg_len)); // input
-  uint8_t* out = static_cast<uint8_t*>(malloc(dig_len));
+  std::vector<uint8_t> msg(msg_len);
+  std::vector<uint8_t> out(dig_len);
 
-  ascon_utils::random_data(msg, msg_len);
+  auto _msg = std::span(msg);
+  auto _out = std::span(out);
 
-  ascon_xof::ascon_xof hasher;
-  hasher.absorb(msg, msg_len);
+  ascon_utils::random_data(_msg);
+
+  ascon_xof::ascon_xof_t hasher;
+  hasher.absorb(_msg);
   hasher.finalize();
+  hasher.squeeze(_out);
 
-  // can request arbitrary many bytes of digest, arbitrary many times
-  for (size_t off = 0; off < dig_len; off += 2) {
-    hasher.read(out + off, 2);
-  }
-
-  std::cout << "Ascon XOF\n\n";
-  std::cout << "Message :\t" << ascon_utils::to_hex(msg, msg_len) << "\n";
-  std::cout << "Digest  :\t" << ascon_utils::to_hex(out, dig_len) << "\n";
-
-  // deallocate resources
-  free(msg);
-  free(out);
+  std::cout << "Ascon Xof\n\n";
+  std::cout << "Message :\t" << ascon_utils::to_hex(_msg) << "\n";
+  std::cout << "Digest  :\t" << ascon_utils::to_hex(_out) << "\n";
 
   return EXIT_SUCCESS;
 }
