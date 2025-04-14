834e81688c0a00c610174c9a1ee89ba14ff5c7ca
Anjan Roy <hello@itzmeanjan.in>
2025-04-14 20:16:11 +0400

    Ascon aead struct supporting incremental encrypt/ decrypt
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/ascon/aead/ascon_aead128.hpp
+++ include/ascon/aead/ascon_aead128.hpp
@@ -1,11 +1,124 @@
 #pragma once
 #include "ascon/aead/duplex.hpp"
+#include "ascon/permutation/ascon.hpp"
+#include "ascon/utils/common.hpp"
+#include "ascon/utils/force_inline.hpp"
+#include <array>
+#include <cstdint>
 
 namespace ascon_aead128 {
 
 static constexpr size_t KEY_BYTE_LEN = ascon_duplex_mode::KEY_BYTE_LEN;
 static constexpr size_t NONCE_BYTE_LEN = ascon_duplex_mode::NONCE_BYTE_LEN;
 static constexpr size_t TAG_BYTE_LEN = ascon_duplex_mode::TAG_BYTE_LEN;
+
+struct ascon_aead128_t
+{
+private:
+  std::array<uint8_t, KEY_BYTE_LEN> key{};
+  std::array<uint8_t, NONCE_BYTE_LEN> nonce{};
+
+  ascon_perm::ascon_perm_t state{};
+  size_t offset = 0;
+  size_t total_absorbed_data_byte_len = 0;
+
+  alignas(4) bool finished_absorbing_data = false;
+  alignas(4) bool finished_ciphering = false;
+  alignas(4) bool finished_squeezing_tag = false;
+
+public:
+  forceinline constexpr ascon_aead128_t(std::span<const uint8_t, KEY_BYTE_LEN> key, std::span<const uint8_t, NONCE_BYTE_LEN> nonce)
+  {
+    ascon_duplex_mode::initialize(state, key, nonce);
+  }
+  forceinline constexpr ~ascon_aead128_t() { this->reset(); }
+
+  [[nodiscard]]
+  forceinline constexpr bool absorb_data(std::span<const uint8_t> data)
+  {
+    if (finished_absorbing_data) {
+      return false;
+    }
+
+    ascon_duplex_mode::absorb_associated_data(state, offset, data);
+    total_absorbed_data_byte_len += data.size();
+
+    return true;
+  }
+
+  [[nodiscard]]
+  forceinline constexpr bool finalize_data()
+  {
+    if (finished_absorbing_data) {
+      return false;
+    }
+
+    ascon_duplex_mode::finalize_associated_data(state, offset, total_absorbed_data_byte_len);
+    finished_absorbing_data = true;
+
+    return true;
+  }
+
+  [[nodiscard]]
+  forceinline constexpr bool encrypt_plaintext(std::span<const uint8_t> plaintext, std::span<uint8_t> ciphertext)
+  {
+    if (!finished_absorbing_data || finished_ciphering) {
+      return false;
+    }
+
+    ascon_duplex_mode::encrypt_plaintext(state, offset, plaintext, ciphertext);
+    return true;
+  }
+
+  [[nodiscard]]
+  forceinline constexpr bool decrypt_plaintext(std::span<const uint8_t> ciphertext, std::span<uint8_t> plaintext)
+  {
+    if (!finished_absorbing_data || finished_ciphering) {
+      return false;
+    }
+
+    ascon_duplex_mode::decrypt_ciphertext(state, offset, ciphertext, plaintext);
+    return true;
+  }
+
+  [[nodiscard]]
+  forceinline constexpr bool finalize_ciphering()
+  {
+    if (!finished_absorbing_data || finished_ciphering) {
+      return false;
+    }
+
+    ascon_duplex_mode::finalize_ciphering(state, offset);
+    finished_ciphering = true;
+
+    return true;
+  }
+
+  [[nodiscard]]
+  forceinline constexpr bool squeeze_tag(std::span<uint8_t, TAG_BYTE_LEN> tag)
+  {
+    if (!finished_absorbing_data || !finished_ciphering || finished_squeezing_tag) {
+      return false;
+    }
+
+    ascon_duplex_mode::finalize(state, key, tag);
+    finished_squeezing_tag = true;
+
+    return true;
+  }
+
+  forceinline constexpr void reset()
+  {
+    this->key.fill(0);
+    this->nonce.fill(0);
+    this->state.reset();
+    this->offset = 0;
+    this->total_absorbed_data_byte_len = 0;
+    this->finished_absorbing_data = false;
+    this->finished_ciphering = false;
+    this->finished_squeezing_tag = false;
+  }
+};
 
 /**
  * @brief Encrypts plaintext using the Ascon-AEAD128 algorithm.
