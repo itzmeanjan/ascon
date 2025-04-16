e70dcb485727a693315cf6e7e758f2bc46607f9b
Anjan Roy <hello@itzmeanjan.in>
2025-04-16 23:28:53 +0400

    Remove legacy oneshot API of Ascon-AEAD128 encrypt/ decrypt
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/ascon/aead/ascon_aead128.hpp
+++ include/ascon/aead/ascon_aead128.hpp
@@ -293,7 +293,7 @@ public:
    *        `TAG_BYTE_LEN`.
    * @return An `ascon_aead128_status_t` indicating the status of the operation:
    *   - `decryption_success_as_tag_matches`: Decryption was successful and the tag matched.
-   *   - `decryption_failure_due_to_tag_mismatch`: Decryption failed because the tag did not match.
+   *   - `decryption_failure_due_to_tag_mismatch`: Decryption failed because the tag did not match. Discard all of previously decrypted plaintext.
    *   - `still_in_data_absorption_phase`: Data absorption phase has not yet been finalized.
    *   - `decryption_phase_already_finalized`: Decryption phase has already been finalized.
    */
@@ -341,84 +341,5 @@ private:
     this->finished_decrypting_ciphertext = false;
   }
 };
-
-/**
- * @brief Encrypts plaintext using the Ascon-AEAD128 algorithm.
- *
- * @param key The 128-bit encryption key.
- * @param nonce The 128-bit nonce (must be unique for each encryption with the same key).
- * @param associated_data Arbitrary-length associated data to be authenticated (but not encrypted).
- * @param plaintext The plaintext to be encrypted.
- * @param ciphertext Output buffer for the ciphertext (must be the same length as plaintext).
- * @param tag Output buffer for the 128-bit authentication tag.
- *
- * This function encrypts the plaintext, producing ciphertext of the same length.
- * It also generates a 128-bit authentication tag that authenticates both the associated data and the ciphertext.
- * The associated data is authenticated but not encrypted.
- */
-forceinline constexpr void
-encrypt(std::span<const uint8_t, KEY_BYTE_LEN> key,
-        std::span<const uint8_t, NONCE_BYTE_LEN> nonce,
-        std::span<const uint8_t> associated_data,
-        std::span<const uint8_t> plaintext,
-        std::span<uint8_t> ciphertext,
-        std::span<uint8_t, TAG_BYTE_LEN> tag)
-{
-  ascon_perm::ascon_perm_t state{};
-
-  ascon_duplex_mode::initialize(state, key, nonce);
-
-  size_t block_offset = 0;
-  ascon_duplex_mode::absorb_associated_data(state, block_offset, associated_data);
-  ascon_duplex_mode::finalize_associated_data(state, block_offset, associated_data.size());
-
-  ascon_duplex_mode::encrypt_plaintext(state, block_offset, plaintext, ciphertext);
-  ascon_duplex_mode::finalize_ciphering(state, block_offset);
-
-  ascon_duplex_mode::finalize(state, key, tag);
-}
-
-/**
- * @brief Decrypts ciphertext using the Ascon-AEAD128 algorithm and verifies its authenticity.
- *
- * @param key The 128-bit encryption key.
- * @param nonce The 128-bit nonce used during encryption.
- * @param associated_data Arbitrary-length associated data used during encryption.
- * @param cipher The ciphertext to be decrypted.
- * @param text Output buffer for the plaintext (must be the same length as cipher).  Will be zeroed if authentication fails.
- * @param tag The 128-bit authentication tag generated during encryption.
- * @return True if the authentication tag is valid and decryption was successful; False otherwise.  If false, the output text will be zeroed.
- *
- * This function decrypts the ciphertext, producing plaintext of the same length.  It also verifies the authenticity of both the ciphertext and associated data
- * using the provided authentication tag. If authentication fails, the function returns false and the plaintext buffer is zeroed.
- */
-[[nodiscard]]
-forceinline constexpr bool
-decrypt(std::span<const uint8_t, KEY_BYTE_LEN> key,
-        std::span<const uint8_t, NONCE_BYTE_LEN> nonce,
-        std::span<const uint8_t> associated_data,
-        std::span<const uint8_t> ciphertext,
-        std::span<uint8_t> plaintext,
-        std::span<const uint8_t, TAG_BYTE_LEN> tag)
-{
-  ascon_perm::ascon_perm_t state{};
-  std::array<uint8_t, TAG_BYTE_LEN> computed_tag{};
-
-  ascon_duplex_mode::initialize(state, key, nonce);
-
-  size_t block_offset = 0;
-  ascon_duplex_mode::absorb_associated_data(state, block_offset, associated_data);
-  ascon_duplex_mode::finalize_associated_data(state, block_offset, associated_data.size());
-
-  ascon_duplex_mode::decrypt_ciphertext(state, block_offset, ciphertext, plaintext);
-  ascon_duplex_mode::finalize_ciphering(state, block_offset);
-
-  ascon_duplex_mode::finalize(state, key, computed_tag);
-
-  const uint32_t flg = ascon_common_utils::ct_eq_byte_array<TAG_BYTE_LEN>(tag, computed_tag);
-  ascon_common_utils::ct_conditional_memset(~flg, plaintext, 0);
-
-  return static_cast<bool>(flg);
-}
 
 }
